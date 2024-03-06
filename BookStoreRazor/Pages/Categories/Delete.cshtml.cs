b95a713c2c9e479b6473b629fd066d16563ebe91
Michael Chukwu <imchukwu@gmail.com>
2024-03-06 04:21:40 +0100

    update latest files

---
+++ BookStoreRazor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using BookStoreRazor.Data;
+using BookStoreRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStoreRazor.Pages.Categories
+{
+
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Category.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category obj = _db.Category.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
